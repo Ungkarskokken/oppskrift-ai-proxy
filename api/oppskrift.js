3281e828a611fa6ea41593292607182210228012
Ungkarskokken <per@ungkarskokken.com>
2025-05-20 11:13:15 +0200

    Commit

--- api/oppskrift.js
+++ api/oppskrift.js
@@ -1,4 +1,4 @@
-eexport default async function handler(req, res) {
+export default async function handler(req, res) {
   // CORS headers
   res.setHeader("Access-Control-Allow-Origin", "https://www.ungkarskokken.com");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
